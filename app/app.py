0d09304450daa63ec1a94136a5662257a9abbd03
Steffen Buergers <sbuergers@gmail.com>
2020-05-29 20:19:00 -0400

    added alpha_vantage package to conda-requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect
+from alpha_vantage.timeseries import TimeSeries
 
 app = Flask(__name__)
+
+
+key = 'KX6JA5U3JFVOOYL9'
+ts = TimeSeries(key)
+aapl, meta = ts.get_daily(symbol='AAPL')
+print(aapl['2019-09-12'])
 
 @app.route('/')
 def index():
